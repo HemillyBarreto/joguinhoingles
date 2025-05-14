33065d7b1267cbd2c1cca3afd76481e6a64dbd6e
Hemilly <hemybarreto3@gmail.com>
2025-05-13 22:05:43 -0400

    Jogo

---
+++ Assets/Scripts/Movimento.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    private float horizontalInput;
+    private Rigidbody2D rb;
+
+    [SerializeField] private int velocidade = 5;
+
+    [SerializeField] private Transform peDoPersonagem;
+    [SerializeField] private LayerMask chaoLayer;
+
+    private bool estaNoChao;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private int movendoHash = Animator.StringToHash("Movendo");
+    private int saltandoHash = Animator.StringToHash("Saltando");
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && estaNoChao)
+        {
+            rb.AddForce(Vector2.up * 600);
+        }
+
+        estaNoChao = Physics2D.OverlapCircle(peDoPersonagem.position, 0.2f, chaoLayer);
+
+        animator.SetBool(movendoHash, horizontalInput != 0);
+        animator.SetBool(saltandoHash, !estaNoChao);
+
+        if(horizontalInput > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if(horizontalInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * velocidade, rb.velocity.y);
+    }
+}
